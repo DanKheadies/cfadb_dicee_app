ee828cee245ad181cb90464b3aaa2463cdb71130
Papa Daco <papadaco@papadaco.attlocal.net>
2021-06-21 12:52:40 -0400

    Flutter plugins dependencies

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Applications/Flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/papadaco/FlutterApps/dicee-flutter"
-export "FLUTTER_TARGET=/Users/papadaco/FlutterApps/dicee-flutter/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/papadaco/FlutterApps/dicee_app"
+export "FLUTTER_TARGET=/Users/papadaco/FlutterApps/dicee_app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
@@ -10,4 +10,4 @@ export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/papadaco/FlutterApps/dicee-flutter/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/papadaco/FlutterApps/dicee_app/.dart_tool/package_config.json"
